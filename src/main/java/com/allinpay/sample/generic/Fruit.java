355c4467b523ab0a0a8ec913e84f109c7184f499
wanglei <wanglei@allinpay.com>
2016-04-26 10:54:08 +0800

    添加泛型方法示例

--- src/main/java/com/allinpay/sample/generic/Fruit.java
+++ src/main/java/com/allinpay/sample/generic/Fruit.java
@@ -1,7 +1,20 @@
 package com.allinpay.sample.generic;
 
+import java.util.List;
+
 /**
  * Created by Angi on 2016/4/21.
  */
 public class Fruit {
+    /**
+     * 泛型方法示例
+     *
+     * @param a
+     * @param <T>
+     * @return
+     */
+    public <T> T a(T a) {
+        T b = a;
+        return b;
+    }
 }
